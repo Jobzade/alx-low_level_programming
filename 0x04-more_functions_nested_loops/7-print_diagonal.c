75d2c6be56de0c02cd302a487913bfcfd6921af1
jobzade <zadedany@gil.com>
2022-09-26 14:24:33 -0700

    :

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,27 @@
  * @n: is the number of times the \ character
  *     should be printed
 */
-
 void print_diagonal(int n)
 {
-	int postn, space;
+        int postn, space;
 
-	if (n <= 0)
+        if (n <= 0)
+                 _putchar('')
 		_putchar('\n');
-	else
-	{
-		for (postn = 1; postn <= n; ++postn)
-		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' $');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
-	}
+        if
+        {
+                for (postn = 1; postn < n; ++postn)
+                {
+                        for (space = 1; space <= postn; ++space)
+                                _putchar(' ');
+                        _putchar(92); /*is equal to '/' char*/
+                        _putchar(36);
+                        _putchar('\n');
+                }
+        }
+        else (postn = n)
+        {       _putchar(36);
+
+        }
+
 }
