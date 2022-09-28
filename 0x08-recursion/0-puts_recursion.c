ad9c53460b2a313131dd9f5cbc702fc9e4a9d2f7
jobzade <zadedany@gil.com>
2022-09-28 13:06:37 -0700

    revision

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
 #include "school.h"
 
 /**
- * _puts_recursion - Print a string
- * @s: string
- */
+ * _puts_recursion - a function that prints a new line
+ *
+ * @s: the string
+ *
+ * Return: nothing
+*/
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(++s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
