4ce8d6fd6da6087375ec96289edd1b5b5f651bc3
jobzade <zadedany@gil.com>
2022-09-30 14:23:11 -0700

    library creation

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "maiin.h"
+
+/**
+ * main - Entry check alx code
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Eyob Wolde");
+	return (0);
+}
