f99c6f4f2408ad38ae4c52cb6c5580370c1ccea4
jobzade <zadedany@gil.com>
2022-10-20 12:25:16 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - calculates the length of a linked list
+ * @h: Pointer to the head of the linked list
+ * Return: The total amount of nodes in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
